9b6bf92f23c7b78d90a9fbc4d482d79a9eb7f48b
Blazej Domagala <blazej.domagala@progtom.com>
2025-12-04 18:50:41 +0100

    Add training script for Patchcore model with data module configuration

---
+++ train.py
@@ -0,0 +1,25 @@
+from anomalib.data import Folder
+from anomalib.models import Patchcore
+from anomalib.engine import Engine
+
+datamodule = Folder(
+    name="wzor_pasek_A",
+    root="./dataset/wzor_pasek_A",
+    normal_dir="train/good",
+    normal_test_dir="test/good",
+    normal_split_ratio=0.2,
+    image_size=(256, 256),
+    train_batch_size=32,
+    eval_batch_size=32,
+    num_workers=8
+)
+
+model = Patchcore(
+    backbone="wide_resnet50_2",        
+    layers=["layer2", "layer3"],       
+    coreset_sampling_ratio=0.1         
+)
+
+engine = Engine()
+engine.fit(model=model, datamodule=datamodule)
+
